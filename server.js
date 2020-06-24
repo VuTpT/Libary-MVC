23da887317bb3f9125f7d7ba4b018c8ee8ae7bed
Glitch (hello-express) <none>
2020-06-24 05:46:51 +0000

    👰🏇 Checkpoint ./server.js:17880600/132 ./views/users/create.pug:17880600/278

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const adapter = new FileSync('db.json');
 const db = low(adapter);
 
 
-db.defaults({ todos: [] }) 
+db.defaults({ todos: []}) 
   .write();
 
 app.set("view engine", "pug");
@@ -28,16 +28,16 @@ app.get('/todos', function(request, response) {
   });
 })
 
-app.get('/todos/search', function(request, response) {
-  var q = request.query.q;
-  var matchedUsers = db.get('todos').filter(function(name){
-    return name.user.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  });
+// app.get('/todos/search', function(request, response) {
+//   var q = request.query.q;
+//   var matchedUsers = db.get('todos').filter(function(name){
+//     return name.user.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+//   });
   
-  response.render('/todos/index.pug', {
-    todos: matchedUsers
-    });
-})
+//   response.render('/views/index', {
+//     todos: matchedUsers
+//     });
+// })
 
 app.get('/todos/create', function(request, response) {
   response.render('users/create');
