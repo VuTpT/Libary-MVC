b1b3a7fd24f1510c22bb463060aca3e48772e99b
Glitch (authentication-15-) <none>
2020-07-24 04:58:37 +0000

    🐘🌔 Checkpoint ./controllers/user.controller.js:17880600/248 ./views/auth/signup.pug:17880600/36

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -56,23 +56,17 @@ module.exports.postUpdate = function(request, response) {
 };
 
 module.exports.postCreate = function(request, response, next) {
-  const saltRounds = bcrypt.genSalt(10);
-
   
   console.log(response.success);
-  
-  bcrypt.hash(request.body.password, saltRounds).then(function(hash) {
     
     db.get('users')
-    .push({ userId : shortid.generate(), email: request.body.email, password: hash, isAdmin: false })
+    .push({ userId : shortid.generate(), name: request.body.name})
     .value()
     .id;
     
   next();
     
   response.render('user/index');
-});
-
 };
 
 
