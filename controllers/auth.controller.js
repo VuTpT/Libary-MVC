a18456b59548bf3fccf6064eeb3258017da57f62
Glitch (authentication-15-) <none>
2020-07-24 06:09:13 +0000

    🚤⚽️ Checkpoint ./controllers/auth.controller.js:17880600/996

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
-
 var md5 = require('md5');
+
 var db = require('../db');
 const bodyParser = require('body-parser');
 const shortid = require('shortid');
@@ -54,24 +54,21 @@ module.exports.postLogin = function(request, response, next) {
   next();
 };
 
-module.exports.postSignUp = function(options, response) {
+module.exports.postSignUp = function(request, response) {
   const saltRounds = bcrypt.genSalt(10);
   var email = db.get('users').map('email').value();
-  var usernameIsTaken = email.includes(options.email)
+  var usernameIsTaken = email.includes(request.email)
   
-//   bcrypt.hash(request.body.password, saltRounds, (err, hash) => {
-//     db.get('users')
-//     .push({ userId : shortid.generate(), email: request.body.email, password: hash, isAdmin: false })
-//     .write();
-// });
   
   if(usernameIsTaken){
-    return response.render(options.signupTemplate, {errors: ['This username is already taken']})
+    return response.render(request.signupTemplate, {errors: ['This username is already taken']})
   }
   else {
+  bcrypt.hash(request.body.password, saltRounds, (err, hash) => {
     db.get('users')
-    .push({ email: request.body.email, password: hashPassword(options.password), isAdmin: false })
+    .push({ userId : shortid.generate(), email: request.body.email, password: hash, isAdmin: false })
     .write();
+    });
   }
   
   response.render('books/index', {
